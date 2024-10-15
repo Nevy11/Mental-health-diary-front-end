0a5ecd490c715c59c9e6ca2fef26536a304fe45c
nevy11 <smongare2003@gmail.com>
2024-10-15 17:59:32 +0300

    update sign up form

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 
 @Component({
   selector: 'diary-sign-up',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, SignUpFormComponent],
   templateUrl: './sign-up.component.html',
-  styleUrl: './sign-up.component.scss'
+  styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {
-
-}
+export class SignUpComponent {}
