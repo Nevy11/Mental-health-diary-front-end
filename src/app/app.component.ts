5e605c5f86bd2c336fca46bcf3d860cf2d164dff
nevy11 <smongare2003@gmail.com>
2024-10-15 16:53:10 +0300

    add sign up title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @Component({
   selector: 'diary-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignUpComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentalHealthDiary';
