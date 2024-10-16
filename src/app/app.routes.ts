b7f7a9b8df7e59dd56ce5bffa1b2d47742286eb9
nevy11 <smongare2003@gmail.com>
2024-10-16 12:20:28 +0300

    add login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
   },
   { path: 'login', component: LoginComponent },
 
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: 'sign up', pathMatch: 'full' },
 ];
 
 @NgModule({
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'diary-sign-up',
   standalone: true,
-  imports: [MatButtonModule, SignUpFormComponent],
+  imports: [MatButtonModule, SignUpFormComponent, RouterModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {}
+export class SignUpComponent {
+  constructor(private router: Router) {}
+  login() {
+    this.router.navigate(['login']);
+  }
+}
