91f266bf0da58eaa32912b084f6474340296334e
nevy11 <smongare2003@gmail.com>
2024-10-17 11:26:04 +0300

    udpate cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { DashDividersComponent } from './dashboard/dash-dividers/dash-dividers.component';
-import { ChatBotDividerComponent } from './dashboard/dash-dividers/chat-bot-divider/chat-bot-divider.component';
-import { DiaryDividerComponent } from './dashboard/dash-dividers/diary-divider/diary-divider.component';
-import { GoalDividerComponent } from './dashboard/dash-dividers/goal-divider/goal-divider.component';
 
 export const routes: Routes = [
   {
@@ -17,18 +13,6 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent,
-  },
-  {
-    path: 'diary divider',
-    component: DiaryDividerComponent,
-  },
-  {
-    path: 'chat bot divider',
-    component: ChatBotDividerComponent,
-  },
-  {
-    path: 'goal divider',
-    component: GoalDividerComponent,
   },
   { path: '', redirectTo: 'sign up', pathMatch: 'full' },
 ];
