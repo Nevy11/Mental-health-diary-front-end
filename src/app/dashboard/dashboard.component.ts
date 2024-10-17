f1a7111b2f4cca3b32e2072c755de5f11873a492
nevy11 <smongare2003@gmail.com>
2024-10-17 16:18:01 +0300

    dashboard content done

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -35,19 +35,30 @@ export class DashboardComponent {
             cols: 1,
             rows: 1,
             mobile: true,
-            card_content:
-              "Talk to me about all of what you are feeling right now and i'll help you. \n Save the cost that you are planning to hire a therapist. \n\n ",
+            card_content: 'Speak or write to mental gpt.',
           },
           {
             title: 'Diary',
             cols: 1,
             rows: 1,
             mobile: true,
-            diary:
-              "Write all of what you are feeling right now to rembember your past. \n Wait, You don't need to type to write all of what you are feeling, we'll do that for you. Just speak as if you are speaking to a real therapist.",
+            diary: 'Note all your feelings, emotions, and behaviour',
           },
-          { title: 'Track Goal', cols: 1, rows: 1, mobile: true },
-          { title: 'Settings', cols: 1, rows: 1, mobile: true },
+          {
+            title: 'Goals',
+            cols: 1,
+            rows: 1,
+            mobile: true,
+            track_goal:
+              'Enter you goals, try improving yourself, both mentally and physically. ',
+          },
+          {
+            title: 'Settings',
+            cols: 1,
+            rows: 1,
+            mobile: true,
+            settings: 'Update your username, email and password',
+          },
         ];
       }
 
@@ -73,9 +84,17 @@ export class DashboardComponent {
           cols: 1,
           rows: 2,
           mobile: false,
-          track_goal: 'The goal is ',
+          track_goal:
+            'Enter you goals, try improving yourself, both mentally and physically. Set your goals, for example, If you have social anxiety, talk to 5 people this week, and in the goal area, if you achieve, carry your goal from todo section to done; then save your update. This enables and helps you to improve your self every day. Success in tracking your goal mate',
         },
-        { title: 'Settings', cols: 1, rows: 1, mobile: false },
+        {
+          title: 'Settings',
+          cols: 1,
+          rows: 1,
+          mobile: false,
+          settings:
+            'view your user settings, update your username, email and password.',
+        },
       ];
     })
   );
