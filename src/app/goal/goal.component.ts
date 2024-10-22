2ffcb4d65f224cdf9574124e469ba2956b20865c
nevy11 <smongare2003@gmail.com>
2024-10-22 18:06:35 +0300

    fill different screen

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { GoalDragNDropComponent } from './goal-drag-n-drop/goal-drag-n-drop.component';
 import { DayRadioComponent } from './day-radio/day-radio.component';
-
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 @Component({
   selector: 'diary-goal',
   standalone: true,
-  imports: [GoalDragNDropComponent, DayRadioComponent],
+  imports: [GoalDragNDropComponent, DayRadioComponent, MatIconModule],
   templateUrl: './goal.component.html',
   styleUrl: './goal.component.scss',
 })
-export class GoalComponent {}
+export class GoalComponent {
+  constructor(private router: Router) {}
+  to_dashboard() {
+    this.router.navigate(['dashboard']);
+  }
+}
