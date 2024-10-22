a535e64a376957ac244e65baa70525eb65046dc2
Arja <arjaooko@gmail.com>
2024-10-22 17:28:43 +0300

    d-D

--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';import {MatInputModule} from '@angular/material/input';
+import {MatFormField, MatFormFieldModule, MatLabel} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatHint } from '@angular/material/form-field';
+
 
 @Component({
   selector: 'diary-diary',
   standalone: true,
-  imports: [],
+  imports: [ MatHint,MatLabel,MatFormField],
   templateUrl: './diary.component.html',
   styleUrl: './diary.component.scss'
 })
