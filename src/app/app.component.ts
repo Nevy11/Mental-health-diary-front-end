95a19bce93a00089a7aea1519b6d4945827b9b2c
nevy11 <smongare2003@gmail.com>
2024-10-22 18:54:56 +0300

    update edges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  name = 'kassy-ux';
+  name = 'Nevy11';
   toggletheme() {
     if (document.body.classList.contains('light-theme')) {
       document.body.classList.remove('light-theme');
