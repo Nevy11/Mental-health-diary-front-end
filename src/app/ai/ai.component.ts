92e1b71ea05a2939cbc8812af1eac6e6d0ee7cd1
kassy-ux <stancyngereso4@gmail.com>
2024-10-26 17:53:07 +0300

    AI done

--- src/app/ai/ai.component.ts
+++ src/app/ai/ai.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'diary-ai',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,FormsModule,],
   templateUrl: './ai.component.html',
   styleUrl: './ai.component.scss'
 })
 export class AIComponent {
+  userInput = '';
+
+  sendMessage() {
+    console.log('Message sent:', this.userInput);
+  
+  }
+
+  startRecording() {
+    console.log('Started recording...');
+    
+  }
+
 
 }
