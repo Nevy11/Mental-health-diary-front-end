e203ba49df019c46160593dbabeaa96b112e4b1a
nevy11 <smongare2003@gmail.com>
2025-02-10 16:53:07 +0300

    modified medium screen username

--- src/app/settings/username-set/username-set.component.ts
+++ src/app/settings/username-set/username-set.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { DataSettingsService } from '../data-settings/data-settings.service';
 import { LoginService } from '../../login/login.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'diary-username-set',
-  imports: [MatButtonModule],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './username-set.component.html',
   styleUrl: './username-set.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UsernameSetComponent implements OnInit {
   constructor(
     private dataSettingsService: DataSettingsService,
     private LoginService: LoginService
-  ) {}
+  ) {
+    merge(this.new_username.statusChanges, this.new_username.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
   username!: string;
   ngOnInit(): void {
     this.dataSettingsService
@@ -26,5 +52,13 @@ export class UsernameSetComponent implements OnInit {
           this.username = resp.username;
         }
       });
+  }
+
+  readonly new_username = new FormControl('', [Validators.required]);
+  error_message = signal('');
+  updateErrorMessage() {
+    if (this.new_username.hasError('required')) {
+      this.error_message.set('Please enter a valid username');
+    }
   }
 }
